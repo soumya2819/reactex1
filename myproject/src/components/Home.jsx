4a6201e60715767eaacf96f8957122a8aee4e5e1
soumya2819 <soumyakorukoppula@gmail.com>
2025-02-18 10:11:23 +0530

    myproject added

---
+++ myproject/src/components/Home.jsx
@@ -0,0 +1,8 @@
+function Home(){
+    return(
+        <div>
+<h2>This is Home.jsx component</h2>
+        </div>
+    )
+}
+export default Home;
---
+++ myproject/src/components/Login.jsx
@@ -0,0 +1,9 @@
+
+function Login(){
+    return(
+        <div>
+<h2>This is login.jsx element</h2>
+        </div>
+    )
+}
+export default Login;
